fd1b9d82553fbb5c39d8a335f50ffae070fb235c
vinay kumar <vkumar8192449@gmail.com>
2024-01-29 01:36:32 +0530

    signup form validations added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,13 +3,27 @@ import { Link } from "react-router-dom";
 const Header = () => {
   return (
     <>
-       <h1>Header</h1>
-        <div class='flex flex-col justify-center items-center'>
-        <Link to="/Signup"><button type="button" class="text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Sign up</button>  </Link>
-        <Link to="/Login"><button type="button" class="text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Login</button>  </Link>
-        </div>
+      <h1>Header</h1>
+      <div className="flex flex-col justify-center items-center">
+        <Link to="/Signup">
+          <button
+            type="button"
+            className="text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+          >
+            Sign up
+          </button>{" "}
+        </Link>
+        <Link to="/Login">
+          <button
+            type="button"
+            className="text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+          >
+            Login
+          </button>{" "}
+        </Link>
+      </div>
     </>
-  )
+  );
 };
 
 export default Header;
--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,134 +1,312 @@
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
-import axios from 'axios';
-import avatar1 from '../assets/avatar1.png'
-import avatar2 from '../assets/avatar2.png'
-import avatar3 from '../assets/avatar3.png'
-import avatar4 from '../assets/avatar4.png'
-import avatar5 from '../assets/avatar5.png'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import avatar1 from "../assets/avatar1.png";
+import avatar2 from "../assets/avatar2.png";
+import avatar3 from "../assets/avatar3.png";
+import avatar4 from "../assets/avatar4.png";
+import avatar5 from "../assets/avatar5.png";
+import toast, { Toaster } from "react-hot-toast";
+import validator from "validator";
 // import EmailVerify from './EmailVerify';
 
 const Signup = () => {
+  const toastId = React.useRef(null);
+  const errornotify = (msg) => toast.error(msg);
+  const sendingnotify = (msg) => (toastId.current = toast.loading(msg));
+  const dismiss = () => toast.dismiss(toastId.current);
+  const successnotify = (msg) => toast.success(msg);
 
-    const [success, setSuccess] = useState(false);
-    const [avatarChoosen, setAvatarChoosen] = useState(1);
-    const [formData, setFormData] = useState({
-        username: '',
-        email: '',
-        password: '',
-        confirmpassword: '',
-        category: 'Open',
-        avatar: avatarChoosen
-    });
-
-    const handleInputChange = (e) => {
-        // console.log(e);
-
-        const { name, value } = e.target;
-        setFormData({ ...formData, [name]: value });
-    }
-
-    const handleSubmit = async (e) => {
-        setFormData({
-            ...formData, avatar: avatarChoosen,
-        });
-        console.log(formData);
-
-        e.preventDefault();
-        try {
-            const response = await axios.post('http://localhost:8000/api/v1/users/register', formData);
-            console.log(response);
-            if (response.data.success) {
-                setSuccess(true);
-            }
-
-        } catch (error) {
-            console.error(error);
+  const [validusername, setvalidusername] = useState("");
+  const checkUsername = async () => {
+    try {
+      const response = await axios.post(
+        "http://localhost:8000/api/v1/users/checkusername",
+        { username: formData.username },
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+          withCredentials: true,
         }
+      );
+
+      if (response.data.message === "valid username") {
+        setvalidusername("username available");
+      }
+    } catch (error) {
+      setvalidusername(error.response.data.message);
     }
+  };
 
-    return (
-        <>
-            <section class='bg-gray-50 flex flex-col items-center justify-center h-screen'>
-                <div class='p-6 space-y-4 bg-white  shadow-md'>
-                    <div class="flex flex-col justify-center items-center">
-                        <div>
-                            <h2 class='text-xl font-bold leading-tight tracking-tight text-gray-900 mb-4'>Choose your Avatar</h2>
-                        </div>
-                        <div class=" flex py-2">
-                            <img src={avatar1} alt="Avatar 1" class={`cursor-pointer border-4 rounded-full transition-transform transform-gpu ${avatarChoosen === 1 ? 'w-12 h-12 mr-2 border-blue-600 hover:scale-110 focus:outline-none' : 'w-11 h-11 mr-2 hover:scale-110'
-                                }`} onClick={() => {
-                                    setAvatarChoosen(1);
-                                }} />
-                            <img src={avatar2} alt="Avatar 1" class={`cursor-pointer border-4 rounded-full transition-transform transform-gpu ${avatarChoosen === 2 ? 'w-12 h-12 mr-2 border-blue-600 hover:scale-110 focus:outline-none' : 'w-11 h-11 mr-2 hover:scale-110'
-                                }`} onClick={() => {
-                                    setAvatarChoosen(2);
-                                }} />
-                            <img src={avatar3} alt="Avatar 1" class={`cursor-pointer border-4 rounded-full transition-transform transform-gpu ${avatarChoosen === 3 ? 'w-12 h-12 mr-2 border-blue-600 hover:scale-110 focus:outline-none' : 'w-11 h-11 mr-2 hover:scale-110'
-                                }`} onClick={() => {
-                                    setAvatarChoosen(3);
-                                }} />
-                            <img src={avatar4} alt="Avatar 1" class={`cursor-pointer border-4 rounded-full transition-transform transform-gpu ${avatarChoosen === 4 ? 'w-12 h-12 mr-2 border-blue-600 hover:scale-110 focus:outline-none' : 'w-11 h-11 mr-2 hover:scale-110'
-                                }`} onClick={() => {
-                                    setAvatarChoosen(4);
-                                }} />
-                            <img src={avatar5} alt="Avatar 1" class={`cursor-pointer border-4 rounded-full transition-transform transform-gpu ${avatarChoosen === 5 ? 'w-12 h-12 mr-2 border-blue-600 hover:scale-110 focus:outline-none' : 'w-11 h-11 mr-2 hover:scale-110'
-                                }`} onClick={() => {
-                                    setAvatarChoosen(5);
-                                }} />
-                        </div>
-                    </div>
-                    <h1 class='text-xl font-bold leading-tight tracking-tight text-gray-900'>
-                        Create your Account
-                    </h1>
-                    <form class='space-y-4' action='#' onSubmit={handleSubmit}>
-                        <div class='flex'>
-                            <div class='flex flex-col mr-4'>
-                                <label for="username" class='block mb-2 text-sm font-medium text-gray-900'>Enter Username</label>
-                                <input type='username' name='username' id='username' placeholder='vikash123' value={formData.username} onChange={handleInputChange} class=' bg-gray-50 border border-gray-300 text-gray-900 rounded-lg block p-2.5' required=''></input>
-                            </div>
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+    password: "1111111",
+    confirmpassword: "1111111",
+    category: "Open",
+    avatar: 1,
+  });
 
-                            <div class='flex flex-col'>
-                                <label for='email' class='block mb-2 text-sm font-medium text-gray-900'>Enter Email</label>
-                                <input type='email' name='email' id='email' placeholder='example@gmail.com' value={formData.email} onChange={handleInputChange} class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg block p-2.5" required=''></input>
-                            </div>
-                        </div>
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+    // if (name === "username") {
+    //   checkUsername();
+    // }
+  };
 
-                        <div class='flex'>
-                            <div class='flex flex-col mr-4'>
-                                <label for='password' class='block mb-2 text-sm font-medium text-gray-900'>Password</label>
-                                <input type='password' name='password' id='password' placeholder='••••••••' value={formData.password} onChange={handleInputChange} class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg block w-full p-2.5 "></input>
-                            </div>
-                            <div class='flex flex-col'>
-                                <label for='password' class='block mb-2 text-sm font-medium text-gray-900'>Confirm Password</label>
-                                <input type='password' name='confirmpassword' id='confirmpassword' placeholder='••••••••' value={formData.confirm_password} onChange={handleInputChange} class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg block w-full p-2.5"></input>
-                            </div>
-                        </div>
-                        <div>
-                            <label for="category" class='block mb-2 text-sm font-medium text-gray-900'>Select Category</label>
-                            <select name='category' id='category' value={formData.category} onChange={handleInputChange} class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg block w-full p-2.5" required=''>
-                                <option value="Open">Open</option>
-                                <option value="Open (PwD)">Open(pwd)</option>
-                                <option value="Gen-EWS">General-Ews</option>
-                                <option value="gen-ews(pwd)">General-Ews(pwd)</option>
-                                <option value="OBC-NCL">Obc-Ncl</option>
-                                <option value="OBC-NCL(PwD)">Obc-Ncl(pwd)</option>
-                                <option value="SC">Sc</option>
-                                <option value="SC(PwD)">Sc(pwd)</option>
-                                <option value="ST">St</option>
-                            </select>
-                        </div>
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!validator.isEmail(formData.email)) {
+      errornotify("Invalid Email");
+      return;
+    }
+    if (formData.password !== formData.confirmpassword) {
+      errornotify("Passwords don't match");
+      return;
+    }
+    sendingnotify("Registering user...");
+    try {
+      const response = await axios.post(
+        "http://localhost:8000/api/v1/users/register",
+        formData,
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+          withCredentials: true,
+        }
+      );
+      dismiss();
+      successnotify(response.data.message);
+      console.log(response.data);
+    } catch (error) {
+      dismiss();
+      errornotify(error.response.data.message);
+      console.error(error.response.data.message);
+    }
+  };
 
-                        <button type="submit" class="w-full bg-blue-600 text-white hover:bg-primary-700 font-medium rounded-lg text-sm px-5 py-2.5 text-center ">Create Account</button>
-                        <p class="text-sm font-light text-gray-500">
-                            Already have an account?
-                            <Link to="/login" class="font-medium text-blue-600 hover:underline">Login here</Link>
-                        </p>
-                    </form>
-                </div>
-            </section>
-        </>
-    )
-}
+  return (
+    <>
+      <Toaster position="bottom-left" reverseOrder={false} />
+      <section className="bg-gray-50 flex flex-col items-center justify-center h-screen">
+        <div className="p-6 space-y-4 bg-white  shadow-md">
+          <div className="flex flex-col justify-center items-center">
+            <div>
+              <h2 className="text-xl font-bold leading-tight tracking-tight text-gray-900 mb-4">
+                Choose your Avatar
+              </h2>
+            </div>
+            <div className=" flex py-2">
+              <img
+                src={avatar1}
+                alt="Avatar 1"
+                className={`cursor-pointer border-4 rounded-full transition-transform transform-gpu ${
+                  formData.avatar === 1
+                    ? "w-12 h-12 mr-2 border-blue-600 hover:scale-110 focus:outline-none"
+                    : "w-11 h-11 mr-2 hover:scale-110"
+                }`}
+                onClick={() => {
+                  setFormData({ ...formData, avatar: 1 });
+                }}
+              />
+              <img
+                src={avatar2}
+                alt="Avatar 1"
+                className={`cursor-pointer border-4 rounded-full transition-transform transform-gpu ${
+                  formData.avatar === 2
+                    ? "w-12 h-12 mr-2 border-blue-600 hover:scale-110 focus:outline-none"
+                    : "w-11 h-11 mr-2 hover:scale-110"
+                }`}
+                onClick={() => {
+                  setFormData({ ...formData, avatar: 2 });
+                }}
+              />
+              <img
+                src={avatar3}
+                alt="Avatar 1"
+                className={`cursor-pointer border-4 rounded-full transition-transform transform-gpu ${
+                  formData.avatar === 3
+                    ? "w-12 h-12 mr-2 border-blue-600 hover:scale-110 focus:outline-none"
+                    : "w-11 h-11 mr-2 hover:scale-110"
+                }`}
+                onClick={() => {
+                  setFormData({ ...formData, avatar: 3 });
+                }}
+              />
+              <img
+                src={avatar4}
+                alt="Avatar 1"
+                className={`cursor-pointer border-4 rounded-full transition-transform transform-gpu ${
+                  formData.avatar === 4
+                    ? "w-12 h-12 mr-2 border-blue-600 hover:scale-110 focus:outline-none"
+                    : "w-11 h-11 mr-2 hover:scale-110"
+                }`}
+                onClick={() => {
+                  setFormData({ ...formData, avatar: 4 });
+                }}
+              />
+              <img
+                src={avatar5}
+                alt="Avatar 1"
+                className={`cursor-pointer border-4 rounded-full transition-transform transform-gpu ${
+                  formData.avatar === 5
+                    ? "w-12 h-12 mr-2 border-blue-600 hover:scale-110 focus:outline-none"
+                    : "w-11 h-11 mr-2 hover:scale-110"
+                }`}
+                onClick={() => {
+                  setFormData({ ...formData, avatar: 5 });
+                }}
+              />
+            </div>
+          </div>
+          <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900">
+            Create your Account
+          </h1>
+          <form className="space-y-4" action="#" onSubmit={handleSubmit}>
+            <div className="flex my-4">
+              <div className="flex flex-col mr-4">
+                <label
+                  htmlFor="username"
+                  className="block mb-2 text-sm font-medium text-gray-900"
+                >
+                  Create Username
+                </label>
+                <input
+                  type="username"
+                  name="username"
+                  id="username"
+                  placeholder="example2024"
+                  value={formData.username}
+                  onChange={handleInputChange}
+                  onBlur={checkUsername}
+                  className={
+                    validusername === "username available"
+                      ? "bg-gray-50 border border-green-300 text-gray-900 rounded-lg block p-2.5"
+                      : validusername === ""
+                      ? "bg-gray-50 border border-grey-300 text-gray-900 rounded-lg block p-2.5"
+                      : "bg-gray-50 border border-red-300 text-gray-900 rounded-lg block p-2.5"
+                  }
+                  required=""
+                ></input>
+                <p
+                  className={
+                    validusername === "username available"
+                      ? "text-xs text-green-700 font-semibold px-1"
+                      : "text-xs text-red-700 font-semibold px-1"
+                  }
+                >
+                  {validusername}
+                </p>
+              </div>
+
+              <div className="flex flex-col">
+                <label
+                  htmlFor="email"
+                  className="block mb-2 text-sm font-medium text-gray-900"
+                >
+                  Enter Email
+                </label>
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  placeholder="example@gmail.com"
+                  value={formData.email}
+                  onChange={handleInputChange}
+                  className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg block p-2.5"
+                  required=""
+                ></input>
+              </div>
+            </div>
+
+            <div className="flex">
+              <div className="flex flex-col mr-4">
+                <label
+                  htmlFor="password"
+                  className="block mb-2 text-sm font-medium text-gray-900"
+                >
+                  Password
+                </label>
+                <input
+                  type="password"
+                  name="password"
+                  id="password"
+                  placeholder="••••••••"
+                  value={formData.password}
+                  onChange={handleInputChange}
+                  className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg block w-full p-2.5 "
+                ></input>
+              </div>
+              <div className="flex flex-col">
+                <label
+                  htmlFor="password"
+                  className="block mb-2 text-sm font-medium text-gray-900"
+                >
+                  Confirm Password
+                </label>
+                <input
+                  type="password"
+                  name="confirmpassword"
+                  id="confirmpassword"
+                  placeholder="••••••••"
+                  value={formData.confirm_password}
+                  onChange={handleInputChange}
+                  className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg block w-full p-2.5"
+                ></input>
+              </div>
+            </div>
+            <div>
+              <label
+                htmlFor="category"
+                className="block mb-2 text-sm font-medium text-gray-900"
+              >
+                Select Category
+              </label>
+              <select
+                name="category"
+                id="category"
+                value={formData.category}
+                onChange={handleInputChange}
+                className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg block w-full p-2.5"
+                required=""
+              >
+                <option value="OBC-NCL">OBC-NCL</option>
+                <option value="ST">ST</option>
+                <option value="OBC-NCL (PwD)">OBC-NCL (PwD)</option>
+                <option value="Gen-EWS">Gen-EWS</option>
+                <option value="Open">Open</option>
+                <option value="Open (PwD)">Open (PwD)</option>
+                <option value="SC">SC</option>
+                <option value="SC (PwD)">SC (PwD)</option>
+                <option value="Gen-EWS (PwD)">Gen-EWS (PwD)</option>
+              </select>
+              {/* ["OBC-NCL", "ST", "OBC-NCL (PwD)", "Gen-EWS", "Open", "Open (PwD)", "SC", "SC (PwD)", "Gen-EWS (PwD)"] */}
+            </div>
+
+            <button
+              type="submit"
+              className="w-full bg-blue-600 text-white hover:bg-primary-700 font-medium rounded-lg text-sm px-5 py-2.5 text-center disabled:opacity-25"
+              disabled={validusername !== "username available"}
+            >
+              Create Account
+            </button>
+            <p className="text-sm font-light text-gray-500">
+              Already have an account?
+              <Link
+                to="/login"
+                className="font-medium text-blue-600 hover:underline"
+              >
+                Login here
+              </Link>
+            </p>
+          </form>
+        </div>
+      </section>
+    </>
+  );
+};
 
 export default Signup;
