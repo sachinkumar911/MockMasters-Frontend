cf44ca2a06f3e70b9b613c44dfd57900e2ad8a18
Vikash Burman <vikasburman37@gmail.com>
2024-01-30 22:10:34 +0530

    header footer contactus componet added

---
+++ src/components/Contact.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const Contact = () => {
+    return (
+        <>
+            <section id='contact-form' class="pt-96">
+                <div class="py-4 px-4 mb-10 mx-auto max-w-screen-md border shadow-lg">
+                    <h2 class="mb-4 text-4xl tracking-tight font-semibold text-center">Contact Us</h2>
+                    {/* <p class="mb-8 lg:mb-16 font-light text-center text-gray-500 sm:text-xl">Got a technical issue? Want to send feedback about a beta feature? Need details about our Business plan? Let us know.</p> */}
+                    <form action="#" class="space-y-8">
+                        <div>
+                            <label for="email" class="block mb-2 text-sm font-medium text-gray-900 ">Your name</label>
+                            <input type="email" id="email" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-gray-500  block w-full p-2.5 focus:ring-opacity-40" placeholder="Enter your Name" required/>
+                        </div>
+                        <div class="sm:col-span-2">
+                            <label for="message" class="block mb-2 text-sm font-medium text-gray-900 ">Your message</label>
+                            <textarea id="message" rows="6" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-gray-500  focus:ring-opacity-40" placeholder="Your Message..."></textarea>
+                        </div>
+                        <button type="submit" class="py-3 px-5 text-sm text-white font-medium text-center rounded-lg bg-gray-800 sm:w-fit hover:bg-opacity-90  focus:outline-none">Send message</button>
+                    </form>
+                </div>
+            </section>
+        </>
+    )
+}
+
+export default Contact
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -27,3 +27,36 @@ const Header = () => {
 };
 
 export default Header;
+
+{/* <header class="header top-0 left-0 w-full">
+                <nav class="bg-white fixed w-full z-20  border-b shadow-sm border-gray-200">
+                    <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-3">
+                        <a href="" class="flex items-center space-x-3">
+                            <span class="self-center text-2xl font-semibold">MockMasters.</span>
+                        </a>
+                        <div class="flex order-2 space-x-3">
+                            <button type="button" class=" font-medium hover:bg-opacity-50 rounded-lg text-base text-center">Login</button>
+                            <p class="mt-1">|</p>
+                            <button type="button" class="text-white bg-gray-800 hover:bg-opacity-90 font-medium rounded-lg text-base px-3 py-2 text-center ">Sign up</button>
+                        </div>
+                        <div class="items-center justify-between flex w-auto " id="">
+                            <ul class="flex p-0 mt-0  font-medium  border-gray-100 rounded-lg  space-x-6 flex-row border-0 bg-white">
+                                <li>
+                                    <a href="#" class="block py-2 px-2 text-gray-900 hover:underline rounded bg-transparent">Home</a>
+                                </li>
+                                <li>
+                                    <a  href="#" class="block py-2 px-2 text-gray-900 hover:underline  rounded bg-transparent">About</a>
+                                </li>
+                                <li>
+                                    <a href="#" class="block py-2 px-2 text-gray-900  hover:underline  rounded bg-transparent">Team</a>
+                                </li>
+                                <li>
+                                    <a href="#" class="block py-2 px-2 text-gray-900 hover:underline  rounded bg-transparent">Contact</a>
+                                </li>
+                            </ul>
+                        </div>
+
+
+                    </div>
+                </nav>
+            </header> */}
