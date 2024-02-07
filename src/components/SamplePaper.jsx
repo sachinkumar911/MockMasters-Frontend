1500a861f9930cc15eeb6c19c75966b53adf2b40
vinay kumar <vkumar8192449@gmail.com>
2024-02-08 00:56:57 +0530

    Sample paper Subjects rendering

--- src/components/SamplePaper.jsx
+++ src/components/SamplePaper.jsx
@@ -1,4 +1,6 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import toast, { Toaster } from "react-hot-toast";
+import axios from "axios";
 //MUI
 import Tabs from "@mui/material/Tabs";
 import Tab from "@mui/material/Tab";
@@ -51,22 +53,50 @@ const AccordionDetails = styled(MuiAccordionDetails)(({ theme }) => ({
 //MUI
 
 const SamplePaper = () => {
+  const [examinfo, setexaminfo] = useState();
+
   //MUI
   const [value, setValue] = useState(0);
+  const ExamTypeArray = ["NIMCET", "TACTM", "JEE"];
 
-  const examhandleChange = (event, newValue) => {
+  const examhandleChange = async (event, newValue) => {
     setValue(newValue);
+    try {
+      const response = await axios.post(
+        "http://localhost:8000/api/v1/examinfo/get-exam",
+        { examtype: ExamTypeArray[newValue] },
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+          withCredentials: true,
+        }
+      );
+      setexaminfo(response.data.data[0]);
+      console.log(response.data.data[0]);
+    } catch (error) {
+      setexaminfo();
+      // console.error(error.response.data.message);
+    }
   };
 
-  const [expanded, setExpanded] = React.useState("panel1");
+  const [expanded, setExpanded] = useState("");
 
-  const handleChange = (panel) => (event, newExpanded) => {
+  const handleChange = (panel, key) => (event, newExpanded) => {
     setExpanded(newExpanded ? panel : false);
+    if (newExpanded) {
+      console.log(examinfo?.markingschema[key].subjectname.subjectname);
+    }
   };
   //MUI
+
+  useEffect(() => {
+    examhandleChange(0, 0);
+  }, []);
 
   return (
     <>
+      <Toaster position="bottom-left" reverseOrder={false} />
       <section id="sample-paper" className=" flex items-center justify-center">
         <div className="container w-[60%] mx-auto">
           <div className="bg-white rounded shadow-lg p-4 px-4 md:p-8 flex flex-col">
@@ -93,151 +123,108 @@ const SamplePaper = () => {
             </div>
 
             <div>
-              <Accordion
-                expanded={expanded === "panel1"}
-                onChange={handleChange("panel1")}
-              >
-                <AccordionSummary
-                  aria-controls="panel1d-content"
-                  id="panel1d-header"
+              {examinfo?.markingschema.map((item, key) => (
+                <Accordion
+                  key={key}
+                  expanded={expanded === `panel${key + 1}`}
+                  onChange={handleChange(`panel${key + 1}`, key)}
                 >
-                  <Typography>Mathematics</Typography>
-                </AccordionSummary>
-                <AccordionDetails>
-                  <Typography>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  </Typography>
-                </AccordionDetails>
-              </Accordion>
-              <Accordion
-                expanded={expanded === "panel2"}
-                onChange={handleChange("panel2")}
-              >
-                <AccordionSummary
-                  aria-controls="panel2d-content"
-                  id="panel2d-header"
-                >
-                  <Typography>
-                    Analytical Ability And Logical Reasoning
-                  </Typography>
-                </AccordionSummary>
-                <AccordionDetails>
-                  <Typography>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  </Typography>
-                </AccordionDetails>
-              </Accordion>
-              <Accordion
-                expanded={expanded === "panel3"}
-                onChange={handleChange("panel3")}
-              >
-                <AccordionSummary
-                  aria-controls="panel3d-content"
-                  id="panel3d-header"
-                >
-                  <Typography>Computer Awareness</Typography>
-                </AccordionSummary>
-                <AccordionDetails>
-                  <Typography>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  </Typography>
-                </AccordionDetails>
-              </Accordion>
-              <Accordion
-                expanded={expanded === "panel4"}
-                onChange={handleChange("panel4")}
-              >
-                <AccordionSummary
-                  aria-controls="panel4d-content"
-                  id="panel4d-header"
-                >
-                  <Typography>General English</Typography>
-                </AccordionSummary>
-                <AccordionDetails>
-                  <div className="overflow-auto max-h-48 overflow-x-hidden ">
-                    <div className="flex items-center my-1 ps-4 border border-gray-200 rounded ">
-                      <input
-                        id="bordered-radio-1"
-                        type="checkbox"
-                        name="bordered-radio"
-                        className="cursor-pointer text-blue-600 w-4 h-4 bg-gray-100 border-gray-300 focus:ring-blue-500"
-                      />
-                      <label
-                        type="text"
-                        className="w-full py-4 ms-2 text-sm font-medium text-gray-900 outline-none"
-                        placeholder="Enter Choice"
-                      >
-                        Solve the following equation for x:2x+5
-                      </label>
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 24 24"
-                        className="w-6 h-6 fill-green-600"
-                      >
-                        <path
-                          fill-rule="evenodd"
-                          d="M4.5 7.5a3 3 0 0 1 3-3h9a3 3 0 0 1 3 3v9a3 3 0 0 1-3 3h-9a3 3 0 0 1-3-3v-9Z"
-                          clip-rule="evenodd"
+                  <AccordionSummary
+                    aria-controls="panel4d-content"
+                    id="panel4d-header"
+                  >
+                    <Typography className="w-full flex justify-between">
+                      <span>{item.subjectname.subjectname}</span>
+                      <span className="font-semibold">
+                        0/{item?.noofquestions}
+                      </span>
+                    </Typography>
+                  </AccordionSummary>
+                  <AccordionDetails>
+                    {/* <div className="overflow-auto max-h-48 overflow-x-hidden ">
+                      <div className="flex items-center my-1 ps-4 border border-gray-200 rounded ">
+                        <input
+                          id="bordered-radio-1"
+                          type="checkbox"
+                          name="bordered-radio"
+                          className="cursor-pointer text-blue-600 w-4 h-4 bg-gray-100 border-gray-300 focus:ring-blue-500"
                         />
-                      </svg>
-                    </div>
-                    <div className="flex items-center my-1 ps-4 border border-gray-200 rounded ">
-                      <input
-                        id="bordered-radio-1"
-                        type="checkbox"
-                        name="bordered-radio"
-                        className="cursor-pointer text-blue-600 w-4 h-4 bg-gray-100 border-gray-300 focus:ring-blue-500"
-                      />
-                      <label
-                        type="text"
-                        className="w-full py-4 ms-2 text-sm font-medium text-gray-900 outline-none"
-                        placeholder="Enter Choice"
-                      >
-                        Solve the following equation for x:2x+5
-                      </label>
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 24 24"
-                        className="w-6 h-6 fill-yellow-500"
-                      >
-                        <path
-                          fill-rule="evenodd"
-                          d="M4.5 7.5a3 3 0 0 1 3-3h9a3 3 0 0 1 3 3v9a3 3 0 0 1-3 3h-9a3 3 0 0 1-3-3v-9Z"
-                          clip-rule="evenodd"
+                        <label
+                          type="text"
+                          className="w-full py-4 ms-2 text-sm font-medium text-gray-900 outline-none"
+                          placeholder="Enter Choice"
+                        >
+                          Solve the following equation for x:2x+5
+                        </label>
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          viewBox="0 0 24 24"
+                          className="w-6 h-6 fill-green-600"
+                        >
+                          <path
+                            fillRule="evenodd"
+                            d="M4.5 7.5a3 3 0 0 1 3-3h9a3 3 0 0 1 3 3v9a3 3 0 0 1-3 3h-9a3 3 0 0 1-3-3v-9Z"
+                            clipRule="evenodd"
+                          />
+                        </svg>
+                      </div>
+                      <div className="flex items-center my-1 ps-4 border border-gray-200 rounded ">
+                        <input
+                          id="bordered-radio-1"
+                          type="checkbox"
+                          name="bordered-radio"
+                          className="cursor-pointer text-blue-600 w-4 h-4 bg-gray-100 border-gray-300 focus:ring-blue-500"
                         />
-                      </svg>
-                    </div>
-                    <div className="flex items-center my-1 ps-4 border border-gray-200 rounded ">
-                      <input
-                        id="bordered-radio-1"
-                        type="checkbox"
-                        name="bordered-radio"
-                        className="cursor-pointer text-blue-600 w-4 h-4 bg-gray-100 border-gray-300 focus:ring-blue-500"
-                      />
-                      <label
-                        type="text"
-                        className="w-full py-4 ms-2 text-sm font-medium text-gray-900 outline-none"
-                        placeholder="Enter Choice"
-                      >
-                        Solve the following equation for x:2x+5
-                      </label>
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 24 24"
-                        className="w-6 h-6 fill-red-600"
-                      >
-                        <path
-                          fill-rule="evenodd"
-                          d="M4.5 7.5a3 3 0 0 1 3-3h9a3 3 0 0 1 3 3v9a3 3 0 0 1-3 3h-9a3 3 0 0 1-3-3v-9Z"
-                          clip-rule="evenodd"
+                        <label
+                          type="text"
+                          className="w-full py-4 ms-2 text-sm font-medium text-gray-900 outline-none"
+                          placeholder="Enter Choice"
+                        >
+                          Solve the following equation for x:2x+5
+                        </label>
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          viewBox="0 0 24 24"
+                          className="w-6 h-6 fill-yellow-500"
+                        >
+                          <path
+                            fillRule="evenodd"
+                            d="M4.5 7.5a3 3 0 0 1 3-3h9a3 3 0 0 1 3 3v9a3 3 0 0 1-3 3h-9a3 3 0 0 1-3-3v-9Z"
+                            clipRule="evenodd"
+                          />
+                        </svg>
+                      </div>
+                      <div className="flex items-center my-1 ps-4 border border-gray-200 rounded ">
+                        <input
+                          id="bordered-radio-1"
+                          type="checkbox"
+                          name="bordered-radio"
+                          className="cursor-pointer text-blue-600 w-4 h-4 bg-gray-100 border-gray-300 focus:ring-blue-500"
                         />
-                      </svg>
-                    </div>
-                  </div>
-                </AccordionDetails>
-              </Accordion>
+                        <label
+                          type="text"
+                          className="w-full py-4 ms-2 text-sm font-medium text-gray-900 outline-none"
+                          placeholder="Enter Choice"
+                        >
+                          Solve the following equation for x:2x+5
+                        </label>
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          viewBox="0 0 24 24"
+                          className="w-6 h-6 fill-red-600"
+                        >
+                          <path
+                            fillRule="evenodd"
+                            d="M4.5 7.5a3 3 0 0 1 3-3h9a3 3 0 0 1 3 3v9a3 3 0 0 1-3 3h-9a3 3 0 0 1-3-3v-9Z"
+                            clipRule="evenodd"
+                          />
+                        </svg>
+                      </div>
+                    </div> */}
+                  </AccordionDetails>
+                </Accordion>
+              ))}
             </div>
-            {/*  */}
           </div>
         </div>
       </section>
