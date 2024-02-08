786b4f98d1d6cc0586f67f238adc9cb99f4f56bc
sachinkumar911 <sachinkumarmehta91@gmail.com>
2024-02-08 08:24:22 +0530

    nav bar well decorated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -21,14 +21,14 @@ const Header = () => {
     <>
       <header className="header top-0 left-0 w-full">
         <nav className="bg-white  w-full z-20  border-b shadow-sm border-gray-200">
-          <div className="flex flex-row justify-around items-center flex-wrap p-3 w-full">
+          <div className="flex flex-row justify-around items-center flex-wrap p-3 w-full  ">
             <NavLink to="/" className="flex items-center ">
               <span className="self-center md:text-2xl  font-semibold">
                 MockMasters.
               </span>
             </NavLink>
 
-            <div className="flex items-center justify-between xl:mr-14 " id="">
+            <div className="flex items-center justify-center   " id="">
               <input
                 type="checkbox"
                 name="hamburger"
@@ -49,12 +49,13 @@ const Header = () => {
                   className="m-auto mt-2 h-0.5 w-6 rounded bg-sky-900 transition duration-300"
                 ></div>
               </label>
-              <div className="peer-checked:translate-x-0 fixed inset-0 w-full translate-x-[-100%] shadow-xl transition duration-300 lg:border-r-0 lg:w-auto lg:static lg:shadow-none lg:translate-x-0 min-[0px]:bg-white lg:bg-transparent  lg:flex  justify-around lg:mr-12">
-                <ul className="px-6 pt-32 text-gray-700 space-y-16 flex flex-col justify-center items-center lg:flex-row md:px-12 lg:space-y-0 lg:flex lg:space-x-16 lg:space-x-reverse lg:pt-0 ">
+              <div className="peer-checked:translate-x-0 fixed inset-0 w-full translate-x-[-100%] shadow-xl transition duration-300 lg:border-r-0 lg:w-auto lg:static lg:shadow-none lg:translate-x-0 min-[0px]:bg-white lg:bg-transparent  lg:flex  lg:pt-0 pt-20 ">
+                <ul className=" text-gray-700 space-y-16 flex flex-col justify-between items-center lg:flex-row  lg:space-y-0 lg:flex lg:space-x-16  ">
                   <li className="">
                     <Link
                       to="/"
-                      className="group relative before:absolute before:inset-x-0 before:bottom-0 before:h-2 hover:cursor-pointer font-semibold text-lg block py-2 px-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent smooth: 'easeInOutQuint'  duration={500} md:hover:text-blue-700 md:p-0 lg:mr-10"
+                      smooth={"easeOutQuint"}
+                      className="group relative before:absolute before:inset-x-0 before:bottom-0 before:h-2 hover:cursor-pointer font-semibold text-lg block py-2 px-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent smooth: 'easeInOutQuint'  duration={500} md:hover:text-blue-700 md:p-0 "
                       onClick={closeNavbar}
                     >
                       Home
@@ -63,6 +64,7 @@ const Header = () => {
                   <li className="">
                     <Link
                       to="about"
+                      smooth={"easeOutQuint"}
                       className="group relative before:absolute before:inset-x-0 before:bottom-0 before:h-2 hover:cursor-pointer font-semibold text-lg block py-2 px-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent smooth: 'easeInOutQuint'  duration={500} md:hover:text-blue-700 md:p-0"
                       onClick={closeNavbar}
                     >
@@ -72,6 +74,7 @@ const Header = () => {
                   <li className="">
                     <Link
                       to="team"
+                      smooth={"easeOutQuint"}
                       className="group relative before:absolute before:inset-x-0 before:bottom-0 before:h-2 hover:cursor-pointer font-semibold text-lg block py-2 px-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent smooth: 'easeInOutQuint'  duration={500} md:hover:text-blue-700 md:p-0"
                       onClick={closeNavbar}
                     >
@@ -81,46 +84,74 @@ const Header = () => {
                   <li className="">
                     <Link
                       to="contact"
-                      smooth={'easeOutQuint'}
-                      className="group relative before:absolute before:inset-x-0 before:bottom-0 before:h-2 hover:cursor-pointer font-semibold text-lg block py-2 px-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent smooth: 'easeInOutQuint'  duration={500} md:hover:text-blue-700 md:p-0 lg:mr-4"
+                      smooth={"easeOutQuint"}
+                      className="group relative before:absolute before:inset-x-0 before:bottom-0 before:h-2 hover:cursor-pointer font-semibold text-lg block py-2 px-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent smooth: 'easeInOutQuint'  duration={500} md:hover:text-blue-700 md:p-0 "
                       onClick={closeNavbar}
                     >
                       Contact
                     </Link>
                   </li>
                 </ul>
-                <div className="lg:p-0 p-12 flex  justify-center ">
-                  <div className="flex flex-col justify-center items-center gap-8 lg:gap-0 lg:flex-row text-lg lg:text-base lg:space-x-8">
-                    {islogin ? (
-                      <NavLink to="/dashboard">
-                        <img
-                          src={avatars[userdetail.avatar - 1]}
-                          alt="Avatar"
-                          className={`cursor-pointer border-4 rounded-full transition-transform transform-gpu md:w-10 md:h-10 w-fit md:mr-2 
+                <div className="flex flex-col justify-center items-center lg:flex-row text-lg lg:text-base lg:gap-4 lg:hidden  pt-14 gap-14">
+              {islogin ? (
+                <NavLink to="/dashboard">
+                  <img
+                    src={avatars[userdetail.avatar - 1]}
+                    alt="Avatar"
+                    className={`cursor-pointer border-4 rounded-full transition-transform transform-gpu md:w-10 md:h-10 w-fit md:mr-2 
                     border-gray-600 hover:scale-110 focus:outline-none`}
-                        />
-                      </NavLink>
-                    ) : (
-                      <>
-                        <button
-                          type="button"
-                          className="lg:font-medium font-semibold
-                    hover:bg-opacity-50 rounded-lg text-base text-center"
-                        >
-                          <NavLink to="/Login">Login</NavLink>
-                        </button>
-                        <p className="hidden lg:block   ">|</p>
-                        <button
-                          type="button"
-                          className="text-white bg-gray-800 hover:bg-opacity-90 md:font-medium rounded-lg md:text-base px-3 py-2 text-center  "
-                        >
-                          <NavLink to="/Signup">Sign up</NavLink>
-                        </button>
-                      </>
-                    )}
-                  </div>
-                </div>
+                  />
+                </NavLink>
+              ) : (
+                <>
+                  <button
+                    type="button"
+                    className="lg:font-medium font-semibold bg-slate-200 px-4 py-2
+                    hover:bg-opacity-50 rounded-lg text-base text-center shadow-md "
+                  >
+                    <NavLink to="/Login">Login</NavLink>
+                  </button>
+                  {/* <p className="hidden lg:block   ">|</p> */}
+                  <button
+                    type="button"
+                    className="text-white bg-gray-800 hover:bg-opacity-90 md:font-medium rounded-lg md:text-base px-3 py-2 text-center  "
+                  >
+                    <NavLink to="/Signup">Sign up</NavLink>
+                  </button>
+                </>
+              )}
+            </div>
               </div>
+            </div>
+
+            <div className=" lg:flex flex-col justify-center items-center lg:flex-row text-lg lg:text-base lg:gap-4  hidden">
+              {islogin ? (
+                <NavLink to="/dashboard">
+                  <img
+                    src={avatars[userdetail.avatar - 1]}
+                    alt="Avatar"
+                    className={`cursor-pointer border-4 rounded-full transition-transform transform-gpu md:w-10 md:h-10 w-fit md:mr-2 
+                    border-gray-600 hover:scale-110 focus:outline-none`}
+                  />
+                </NavLink>
+              ) : (
+                <>
+                  <button
+                    type="button"
+                    className="lg:font-medium font-semibold bg-slate-200 px-4 py-2
+                    hover:bg-opacity-50 rounded-lg text-base text-center shadow-md "
+                  >
+                    <NavLink to="/Login">Login</NavLink>
+                  </button>
+                  {/* <p className="hidden lg:block   ">|</p> */}
+                  <button
+                    type="button"
+                    className="text-white bg-gray-800 hover:bg-opacity-90 md:font-medium rounded-lg md:text-base px-3 py-2 text-center  "
+                  >
+                    <NavLink to="/Signup">Sign up</NavLink>
+                  </button>
+                </>
+              )}
             </div>
           </div>
         </nav>
