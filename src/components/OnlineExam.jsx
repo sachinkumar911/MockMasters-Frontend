5defaf63ca215e32988f33c1db54bc3701cb8ad2
sachinkumar911 <sachinkumarmehta91@gmail.com>
2024-02-15 20:17:58 +0530

    modifying online exam component

--- src/components/OnlineExam.jsx
+++ src/components/OnlineExam.jsx
@@ -64,10 +64,10 @@ const OnlineExam = () => {
   return (
     <>
       <section>
-        <div className="bg-gray-200 h-[10vh] flex items-center justify-center">
+        <div className="h-[8vh] py-4 flex items-center justify-center">
           {AllData?.qpname}
         </div>
-        <div className="h-[10vh] bg-red-200 flex items-center justify-center">
+        {/* <div className="h-fit py-4 bg-red-200 flex items-center justify-center">
           <select
             onChange={(e) => {
               changesubjectpanel(e);
@@ -80,9 +80,60 @@ const OnlineExam = () => {
               </option>
             ))}
           </select>
+        </div> */}
+        <div className=" bg-gray-100  h-fit ">
+          <div className="sm:hidden flex justify-between ">
+            <label htmlFor="Tab" className="sr-only">
+              Tab
+            </label>
+
+            <select id="Tab" className="w-1/2 rounded-md border-gray-200">
+              <option>Mathematics</option>
+              <option>Logical Reasoning</option>
+              <option>Computer</option>
+              <option>English</option>
+            </select>
+          </div>
+
+          <div className="hidden sm:block">
+            <div className=" ">
+              <nav className="flex  justify-between items-center mx-20">
+                <div className="flex -mb-px  gap-3 ">
+                  <button
+                    href="#"
+                    className="shrink-0 border border-transparent p-3 text-sm font-medium text-gray-500 hover:text-gray-700"
+                  >
+                    Mathematics
+                  </button>
+
+                  <button
+                    href="#"
+                    className="shrink-0 border border-transparent p-3 text-sm font-medium text-gray-500 hover:text-gray-700"
+                  >
+                    Logical Reasoning
+                  </button>
+
+                  <button
+                    href="#"
+                    className="shrink-0 border border-transparent p-3 text-sm font-medium text-gray-500 hover:text-gray-700"
+                  >
+                    Computer
+                  </button>
+
+                  <button
+                    href="#"
+                    className="shrink-0 rounded-t-lg border  border-transparent text-sm font-medium text-sky-600"
+                  >
+                    English
+                  </button>
+                </div>
+              </nav>
+            </div>
+          </div>
         </div>
-        <div className="flex">
-          <div className="h-[70vh] bg-green-200 w-[75vw]">
+
+        <div className="flex justify-center items-center mx-24">
+          <div className="h-[77vh] bg-green-200 lg:w-[75vw] w-full ">
             <div>{currentDisplay?.txtquestion}</div>
             {currentDisplay?.options?.map((item, key) => (
               <div key={key}>
@@ -95,7 +146,25 @@ const OnlineExam = () => {
               </div>
             ))}
           </div>
-          <div className="h-[70vh] bg-yellow-200 w-[25vw]">
+
+          <div className="h-[77vh]  border-x border-y border-black bg-white w-[25vw]  hidden lg:flex  lg:flex-col ">
+            <div className=" flex flex-col gap-2 w-full justify-center  mt-2  ml-3 ">
+              <div className=" flex  gap-2 ">
+                <div className=" h-6 w-6 bg-green-400"></div>
+                Answered
+              </div>
+
+              <div className=" flex  gap-2 ">
+                <div className=" h-6 w-6 bg-orange-400 "></div>
+                Not Answered
+              </div>
+
+              <div className=" flex  gap-2 ">
+                <div className=" h-6 w-6 bg-gray-300"></div>
+                Not visited
+              </div>
+            </div>
+
             <div className="flex flex-wrap">
               {currentPanel?.map((item, key) => (
                 <div
@@ -111,17 +180,41 @@ const OnlineExam = () => {
             </div>
           </div>
         </div>
-        <div className="h-[10vh] bg-blue-400 flex items-center justify-evenly">
-          <button className="border-2 border-white rounded-md py-1 px-3 bg-blue-600">
-            prev
+        <div className="h-fit py-4 bg-gray-200 flex items-center justify-evenly">
+          <button
+            className={`px-4 py-2 rounded transition-colors duration-300
+                hover:bg-blue-700 bg-white text-black font-semibold`}
+            id="save"
+          >
+            Previous
           </button>
-          <button className="border-2 border-white rounded-md py-1 px-3 bg-blue-600">
-            mark for review
+          <button
+            className={`px-4 py-2 rounded transition-colors duration-300
+              hover:bg-blue-700 bg-gray-800 text-white font-semibold`}
+            id="review"
+          >
+            Mark for Review &amp; Next
           </button>
-          <button className="border-2 border-white rounded-md py-1 px-3 bg-blue-600">
-            next
+          <button
+            className={`px-4
+                py-2
+                rounded
+                transition-colors
+                duration-300
+                hover:bg-blue-700 bg-white font-semibold`}
+            id="save"
+          >
+            Save &amp; Next
           </button>
-          <button className="border-2 border-white rounded-md py-1 px-3 bg-blue-600">
+          <button
+            className={`px-4
+              py-2
+              rounded
+              transition-colors
+              duration-300
+              hover:bg-blue-700   bg-green-500 font-semibold`}
+            id="submit"
+          >
             Submit
           </button>
         </div>
