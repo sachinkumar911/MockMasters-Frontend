f44146f80a17e177172cb95209fbc6a850a3318e
sachinkumar911 <sachinkumarmehta91@gmail.com>
2024-02-16 12:27:26 +0530

    modifying online exam section

--- src/components/OnlineExam.jsx
+++ src/components/OnlineExam.jsx
@@ -70,12 +70,21 @@ const OnlineExam = () => {
   return (
     <>
       <section>
-        <div className="h-[8vh] py-4 flex items-center justify-center text-xl font-semibold">
-          {AllData?.qpname}
+        <div className=" h-fit flex justify-between items-center mx-[6rem] ">
+          <div className="self-center md:text-2xl  font-semibold">
+            MockMasters.
+          </div>
+          <div className=" py-4 flex items-center justify-center md:text-xl font-semibold">
+            {AllData?.qpname}
+          </div>
+          <div className=" py-[0.5px] pr-1 rounded-full self-center text-xl font-semibold h-8  w-8 bg-slate-300 px-3">
+            S
+          </div>
         </div>
-        <div className="max-h-[8vh] bg-red-200 flex items-center px-24">
+
+        <div className="max-h-[8vh] bg-gray-200 flex justify-between items-center px-24">
           <Box
-            className="lg:w-[65vw]"
+            className="lg:w-fit" /*-[65vw] to  making it fit for timer component temporary*/
             sx={{
               bgcolor: "#fafafa",
             }}
@@ -96,38 +105,122 @@ const OnlineExam = () => {
               ))}
             </Tabs>
           </Box>
+
+          <div className="self-center md:text-xl font-semibold">
+            Time <span className=" self-center">00:00</span>
+          </div>
         </div>
 
-        <div className="flex justify-center items-center mx-24">
-          <div className="h-[77vh] bg-green-200 lg:w-[75vw] w-full ">
-            <div>{currentDisplay?.txtquestion}</div>
+        <div className="flex h-[77vh] justify-center items-center mx-24">
+          <div className=" h-full flex flex-col  lg:w-[75vw] w-full ">
+            <div className=" h-fit flex justify-between items-center  border-b-2 border-slate-300 mb-1 ">
+              <h2 className="py-3 px-2 font-semibold">Question 8</h2>
+              <h2 className="py-3 px-2 font-semibold">
+                {" "}
+                Single correct option,
+                <span className="text-green-400 ">+12.0</span>{" "}
+                <span className="text-red-500 ">-3.0</span>
+              </h2>
+            </div>
+
+            <div className="px-2 lg:text-lg h-[60%]  ">
+              {currentDisplay?.txtquestion}
+            </div>
             {currentDisplay?.options?.map((item, key) => (
-              <div key={key}>
+              <div key={key} className="px-2 lg:text-lg">
                 <input
                   type="radio"
                   name={currentDisplay?._id}
                   id={currentDisplay?._id + key}
+                  className=" h-6 w-6 bg-slate-200 "
                 />
                 <label htmlFor={currentDisplay?._id + key}>{item}</label>
               </div>
             ))}
+            <div className=" w-full   ">
+              <div
+                className={`flex flex-col space-x-4 px-4 py-2 justify-between  `}
+              >
+                <div className=" ms-2  flex bg-slate-50  py-3 px-2 justify-between">
+                  <button
+                    className={`px-4
+              py-2
+              rounded
+              transition-colors
+              duration-300
+              hover:bg-blue-700 bg-green-500`}
+                    id="review"
+                  >
+                    Mark for Review &amp; Next
+                  </button>
+                  <button
+                    className={`px-4
+              py-2
+              rounded
+              transition-colors
+              duration-300
+              hover:bg-blue-700 bg-indigo-700`}
+                    id="clear"
+                  >
+                    Clear Response
+                  </button>
+                </div>
+                <div className="flex justify-between items-center mt-4 bg-slate-50 py-3 px-2 ">
+                  <div className="flex gap-2 justify-center items-center">
+                    <button
+                      className={`px-4 py-2 rounded transition-colors duration-300
+                hover:bg-blue-700 bg-white text-black font-semibold`}
+                      id="save"
+                    >
+                      Previous
+                    </button>
+                    <button
+                      className={`px-4
+                py-2
+                rounded
+                transition-colors
+                duration-300
+                hover:bg-blue-700 bg-sky-800`}
+                      id="save"
+                    >
+                      Save &amp; Next
+                    </button>
+                  </div>
+                  <button
+                    className={`px-4
+              py-2
+              rounded
+              transition-colors
+              duration-300
+              hover:bg-blue-700 bg-cyan-400`}
+                    id="submit"
+                  >
+                    Submit
+                  </button>
+                </div>
+              </div>
+            </div>
           </div>
 
-          <div className="h-[77vh]  border-x border-y border-black bg-white w-[25vw]  hidden lg:flex  lg:flex-col ">
-            <div className=" flex flex-col gap-2 w-full justify-center  mt-2  ml-3 ">
-              <div className=" flex  gap-2 ">
+          <div className="h-full border-x border-y border-black bg-white w-[25vw]  hidden lg:flex  lg:flex-col ">
+            <div className=" flex  flex-wrap w-full justify-between items-center   my-2  px-5">
+              <div className=" flex  gap-2 py-2 text-sm  ">
                 <div className=" h-6 w-6 bg-green-400"></div>
                 Answered
               </div>
 
-              <div className=" flex  gap-2 ">
-                <div className=" h-6 w-6 bg-orange-400 "></div>
+              <div className=" flex  gap-2 py-2 text-sm  pr-4 ">
+                <div className=" h-6 w-6 bg-orange-400  "></div>
                 Not Answered
               </div>
 
-              <div className=" flex  gap-2 ">
+              <div className=" flex  gap-2 py-2 text-sm  ">
                 <div className=" h-6 w-6 bg-gray-300"></div>
                 Not visited
+              </div>
+              <div className=" flex  gap-2 py-2 text-sm ">
+                <div className=" h-6 w-6 bg-pink-500"></div>
+                Mark for reviwed
               </div>
             </div>
 
@@ -138,7 +231,7 @@ const OnlineExam = () => {
                   onClick={() => {
                     changeDisplay(key);
                   }}
-                  className="w-5 h-5 bg-blue-900 m-2 flex justify-center items-center rounded-sm"
+                  className="w-6 h-6 bg-gray-300 m-2 flex justify-center items-center rounded-sm"
                 >
                   {key + 1}
                 </div>
@@ -146,7 +239,7 @@ const OnlineExam = () => {
             </div>
           </div>
         </div>
-        <div className="h-fit py-4 bg-gray-200 flex items-center justify-evenly">
+        {/* <div className="h-fit py-4 bg-gray-200 flex items-center justify-evenly">
           <button
             className={`px-4 py-2 rounded transition-colors duration-300
                 hover:bg-blue-700 bg-white text-black font-semibold`}
@@ -183,7 +276,7 @@ const OnlineExam = () => {
           >
             Submit
           </button>
-        </div>
+        </div> */}
       </section>
     </>
   );
