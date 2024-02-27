ba845727a4497eadfec8065a5d3536d596001fba
vinay kumar <vkumar8192449@gmail.com>
2024-02-28 03:38:59 +0530

    Refferal Code working

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -51,6 +51,7 @@ const Signup = () => {
     confirmpassword: "",
     category: "Open",
     avatar: 1,
+    refferalcode: "",
   });
 
   const handleInputChange = (e) => {
@@ -92,6 +93,7 @@ const Signup = () => {
       sessionStorage.setItem("confirmpassword", formData.confirmpassword);
       sessionStorage.setItem("category", formData.category);
       sessionStorage.setItem("avatar", formData.avatar);
+      sessionStorage.setItem("refferalcode", formData.refferalcode);
       setTimeout(() => {
         Navigate("/emailverify");
       }, 1000);
@@ -108,10 +110,9 @@ const Signup = () => {
       <section className="bg-gray-100 flex flex-col items-center justify-center min-h-[90vh]  py-4">
         <div className="p-6 max-sm:p-3  bg-white  shadow-md max-sm:w-[95%] ">
           <div className="flex flex-col justify-center items-center">
-
-              <h2 className="text-xl font-bold  tracking-tight text-gray-900 mb-3">
-                Choose your Avatar
-              </h2>
+            <h2 className="text-xl font-bold  tracking-tight text-gray-900 mb-3">
+              Choose your Avatar
+            </h2>
 
             <div className=" flex py-2 flex-wrap justify-center">
               <img
@@ -180,158 +181,160 @@ const Signup = () => {
             Create your Account
           </h1>
           <form className="space-y-2" action="#" onSubmit={handleSubmit}>
-          <div id='form-div' className="space-y-2 max-sm:space-y-4 ">
-            <div className="flex  justify-between max-sm:space-x-0 space-x-4 max-sm:flex-col max-sm:space-y-2  ">
-              <div className="flex flex-col">
-                <label
-                  htmlFor="username"
-                  className="block mb-1 text-sm font-medium text-gray-900"
-                >
-                  Create Username
-                </label>
-                <input
-                  type="username"
-                  name="username"
-                  id="username"
-                  placeholder="example2024"
-                  value={formData.username}
-                  onChange={handleInputChange}
-                  onBlur={checkUsername}
-                  className={
-                    validusername === "username available"
-                      ? "bg-gray-50 border border-green-300 text-gray-900 rounded-lg block p-2.5"
-                      : validusername === ""
-                      ? "bg-gray-50 border border-grey-300 text-gray-900 rounded-lg block p-2.5"
-                      : "bg-gray-50 border border-red-300 text-gray-900 rounded-lg block p-2.5"
-                  }
-                  required=""
-                ></input>
-                <p
-                  className={
-                    validusername === "username available"
-                      ? "text-xs text-green-700 font-semibold px-1"
-                      : "text-xs text-red-700 font-semibold px-1"
-                  }
-                >
-                  {validusername}
-                </p>
+            <div id="form-div" className="space-y-2 max-sm:space-y-4 ">
+              <div className="flex  justify-between max-sm:space-x-0 space-x-4 max-sm:flex-col max-sm:space-y-2  ">
+                <div className="flex flex-col">
+                  <label
+                    htmlFor="username"
+                    className="block mb-1 text-sm font-medium text-gray-900"
+                  >
+                    Create Username
+                  </label>
+                  <input
+                    type="username"
+                    name="username"
+                    id="username"
+                    placeholder="example2024"
+                    value={formData.username}
+                    onChange={handleInputChange}
+                    onBlur={checkUsername}
+                    className={
+                      validusername === "username available"
+                        ? "bg-gray-50 border border-green-300 text-gray-900 rounded-lg block p-2.5"
+                        : validusername === ""
+                        ? "bg-gray-50 border border-grey-300 text-gray-900 rounded-lg block p-2.5"
+                        : "bg-gray-50 border border-red-300 text-gray-900 rounded-lg block p-2.5"
+                    }
+                    required=""
+                  ></input>
+                  <p
+                    className={
+                      validusername === "username available"
+                        ? "text-xs text-green-700 font-semibold px-1"
+                        : "text-xs text-red-700 font-semibold px-1"
+                    }
+                  >
+                    {validusername}
+                  </p>
+                </div>
+
+                <div className="flex flex-col">
+                  <label
+                    htmlFor="email"
+                    className="block mb-1 text-sm font-medium text-gray-900"
+                  >
+                    Enter Email
+                  </label>
+                  <input
+                    type="email"
+                    name="email"
+                    id="email"
+                    placeholder="example@gmail.com"
+                    value={formData.email}
+                    onChange={handleInputChange}
+                    className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg block p-2.5"
+                    required=""
+                  ></input>
+                </div>
               </div>
 
-              <div className="flex flex-col">
-                <label
-                  htmlFor="email"
-                  className="block mb-1 text-sm font-medium text-gray-900"
-                >
-                  Enter Email
-                </label>
-                <input
-                  type="email"
-                  name="email"
-                  id="email"
-                  placeholder="example@gmail.com"
-                  value={formData.email}
-                  onChange={handleInputChange}
-                  className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg block p-2.5"
-                  required=""
-                ></input>
+              <div className="flex  justify-between  max-sm:flex-col ">
+                <div className="flex flex-col ">
+                  <label
+                    htmlFor="password"
+                    className="block mb-1 text-sm font-medium text-gray-900"
+                  >
+                    Password
+                  </label>
+                  <input
+                    type="password"
+                    name="password"
+                    id="password"
+                    placeholder="••••••••"
+                    value={formData.password}
+                    onChange={handleInputChange}
+                    className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg block w-full p-2.5 "
+                  ></input>
+                </div>
+                <div className="flex flex-col">
+                  <label
+                    htmlFor="password"
+                    className="block mb-1 text-sm font-medium text-gray-900"
+                  >
+                    Confirm Password
+                  </label>
+                  <input
+                    type="password"
+                    name="confirmpassword"
+                    id="confirmpassword"
+                    placeholder="••••••••"
+                    value={formData.confirm_password}
+                    onChange={handleInputChange}
+                    className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg block w-full p-2.5"
+                  ></input>
+                </div>
               </div>
-            </div>
-
-            <div className="flex  justify-between  max-sm:flex-col ">
               <div className="flex flex-col ">
                 <label
-                  htmlFor="password"
+                  htmlFor="category"
                   className="block mb-1 text-sm font-medium text-gray-900"
                 >
-                  Password
+                  Select Category
                 </label>
-                <input
-                  type="password"
-                  name="password"
-                  id="password"
-                  placeholder="••••••••"
-                  value={formData.password}
-                  onChange={handleInputChange}
-                  className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg block w-full p-2.5 "
-                ></input>
-              </div>
-              <div className="flex flex-col">
-                <label
-                  htmlFor="password"
-                  className="block mb-1 text-sm font-medium text-gray-900"
-                >
-                  Confirm Password
-                </label>
-                <input
-                  type="password"
-                  name="confirmpassword"
-                  id="confirmpassword"
-                  placeholder="••••••••"
-                  value={formData.confirm_password}
+                <select
+                  name="category"
+                  id="category"
+                  value={formData.category}
                   onChange={handleInputChange}
                   className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg block w-full p-2.5"
-                ></input>
+                  required=""
+                >
+                  <option value="OBC-NCL">OBC-NCL</option>
+                  <option value="ST">ST</option>
+                  <option value="OBC-NCL (PwD)">OBC-NCL (PwD)</option>
+                  <option value="Gen-EWS">Gen-EWS</option>
+                  <option value="Open">Open</option>
+                  <option value="Open (PwD)">Open (PwD)</option>
+                  <option value="SC">SC</option>
+                  <option value="SC (PwD)">SC (PwD)</option>
+                  <option value="Gen-EWS (PwD)">Gen-EWS (PwD)</option>
+                </select>
+                {/* ["OBC-NCL", "ST", "OBC-NCL (PwD)", "Gen-EWS", "Open", "Open (PwD)", "SC", "SC (PwD)", "Gen-EWS (PwD)"] */}
               </div>
-            </div>
-            <div className="flex flex-col ">
-              <label
-                htmlFor="category"
-                className="block mb-1 text-sm font-medium text-gray-900"
-              >
-                Select Category
-              </label>
-              <select
-                name="category"
-                id="category"
-                value={formData.category}
-                onChange={handleInputChange}
-                className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg block w-full p-2.5"
-                required=""
-              >
-                <option value="OBC-NCL">OBC-NCL</option>
-                <option value="ST">ST</option>
-                <option value="OBC-NCL (PwD)">OBC-NCL (PwD)</option>
-                <option value="Gen-EWS">Gen-EWS</option>
-                <option value="Open">Open</option>
-                <option value="Open (PwD)">Open (PwD)</option>
-                <option value="SC">SC</option>
-                <option value="SC (PwD)">SC (PwD)</option>
-                <option value="Gen-EWS (PwD)">Gen-EWS (PwD)</option>
-              </select>
-              {/* ["OBC-NCL", "ST", "OBC-NCL (PwD)", "Gen-EWS", "Open", "Open (PwD)", "SC", "SC (PwD)", "Gen-EWS (PwD)"] */}
-            </div>
-            <div class="flex flex-col">
-              <label
-                for="email"
-                class="block mb-1 text-sm font-medium text-gray-900"
-              >
-                Referral Code if Any
-              </label>
-              <input
-                type="email"
-                id="email"
-                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg  block w-full p-2.5 "
-                placeholder="ABYEIUP"
-                required
-              />
-            </div>
+              <div class="flex flex-col">
+                <label
+                  for="email"
+                  class="block mb-1 text-sm font-medium text-gray-900"
+                >
+                  Referral Code if Any
+                </label>
+                <input
+                  name="refferalcode"
+                  type="text"
+                  id="rffr"
+                  class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg  block w-full p-2.5 "
+                  placeholder="ABYEIUP"
+                  value={formData.refferalcode.toUpperCase()}
+                  onChange={handleInputChange}
+                />
+              </div>
 
-            <button
-              type="submit"
-              className="w-full  text-white bg-gray-700 hover:bg-gray-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center disabled:opacity-25"
-              disabled={validusername !== "username available"}
-            >
-              Create Account
-            </button>
-            <p className="text-sm font-light text-gray-500">
-              Already have an account?
-              <Link
-                to="/login"
-                className="ml-1 font-medium text-gray-600 hover:underline"
+              <button
+                type="submit"
+                className="w-full  text-white bg-gray-700 hover:bg-gray-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center disabled:opacity-25"
+                disabled={validusername !== "username available"}
               >
-                Login here
-              </Link>
-            </p>
+                Create Account
+              </button>
+              <p className="text-sm font-light text-gray-500">
+                Already have an account?
+                <Link
+                  to="/login"
+                  className="ml-1 font-medium text-gray-600 hover:underline"
+                >
+                  Login here
+                </Link>
+              </p>
             </div>
           </form>
         </div>
