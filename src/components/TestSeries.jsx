d11d1ba269fa176aa2bb358b29784edfa1432fbd
vinay kumar <vkumar8192449@gmail.com>
2024-05-11 19:21:15 +0530

    Spinner added in Dashboard

--- src/components/TestSeries.jsx
+++ src/components/TestSeries.jsx
@@ -10,6 +10,7 @@ import CssBaseline from "@mui/material/CssBaseline";
 import Box from "@mui/material/Box";
 import Modal from "@mui/material/Modal";
 import { Link } from "react-router-dom";
+import LoadingSpinner from "./LoadingSpinner.jsx";
 
 const style = {
   position: "absolute",
@@ -79,141 +80,145 @@ const TestSeries = () => {
 
   return (
     <>
-      <section id="test-section" className={`my-10 z-0`}>
-        <div className="flex flex-col  justify-center items-center space-y-6 ">
-          {examsets?.map((item, key) => (
-            <div
-              key={key}
-              id={item.qpname}
-              className=" rounded-lg relative overflow-hidden bg-white border-t shadow-md  lg:w-[600px] md:w-[75%] w-[90%] "
-            >
-              <div className="absolute right-0 top-0 h-16 w-16">
-                <div
-                  className={
-                    item.paperprice === 0
-                      ? "absolute transform rotate-45 bg-yellow-400 text-center max-sm:text-[13px] text-white font-semibold py-1 max-sm:py-0 right-[-35px] max-sm:right-[-55px]  top-[32px] w-[170px]"
-                      : "absolute transform flex items-center gap-1 justify-center rotate-45 max-sm:text-[13px] bg-gray-600 text-center text-white font-semibold py-1 max-sm:py-0 max-sm:right-[-55px]  right-[-35px] top-[32px] w-[170px]"
-                  }
-                >
-                  {item.paperprice === 0 ? (
-                    "FREE"
-                  ) : (
-                    <>
-                      {item.paperprice}
-                      <img
-                        src={coinimage}
-                        alt="coinimage"
-                        className="h-4 w-4"
-                      ></img>
-                    </>
-                  )}
-                </div>
-              </div>
-              <h5 className=" border-neutral-100 px-6 pt-2 md:text-xl text-sm font-medium leading-tight">
-                {item.qpname}
-              </h5>
-              <div className="p-5 max-sm:p-3  flex  md:text-lg text-xs">
-                <p className="px-1">{item.noofquestions} Questions</p>
-                <p className="px-1 text-gray-400">|</p>
-                <p className="px-1">{item.totalmarks} marks</p>
-                <p className="px-1 text-gray-400">|</p>
-                <p className="px-1">
-                  {humanizeDuration(item.examinfo?.duration)}
-                </p>
-              </div>
-              <div className="mx-6 mb-3  flex items-center justify-between max-sm:flex-col max-sm:items-center   gap-2">
-                <div className="flex">
-                  <button
-                    onClick={() => startTest(item)}
-                    type="button"
-                    className="inline-block rounded text-white bg-blue-500 hover:bg-blue-600 border-blue-500  border-2 px-4 py-2 text-sm max-sm:text-[10px] max-sm:p-1 leading-normal"
+      {examsets ? (
+        <section id="test-section" className={`my-10 z-0`}>
+          <div className="flex flex-col  justify-center items-center space-y-6 ">
+            {examsets?.map((item, key) => (
+              <div
+                key={key}
+                id={item.qpname}
+                className=" rounded-lg relative overflow-hidden bg-white border-t shadow-md  lg:w-[600px] md:w-[75%] w-[90%] "
+              >
+                <div className="absolute right-0 top-0 h-16 w-16">
+                  <div
+                    className={
+                      item.paperprice === 0
+                        ? "absolute transform rotate-45 bg-yellow-400 text-center max-sm:text-[13px] text-white font-semibold py-1 max-sm:py-0 right-[-35px] max-sm:right-[-55px]  top-[32px] w-[170px]"
+                        : "absolute transform flex items-center gap-1 justify-center rotate-45 max-sm:text-[13px] bg-gray-600 text-center text-white font-semibold py-1 max-sm:py-0 max-sm:right-[-55px]  right-[-35px] top-[32px] w-[170px]"
+                    }
                   >
-                    {item.isResume ? "Start new test" : "Start Test"}
-                  </button>
-                  {item.isResume ? (
-                    <>
-                      {" "}
-                      <button
-                        onClick={() => checkCoins(item, 50)}
-                        type="button"
-                        className="flex rounded text-blue-500 bg-white border-blue-500 border-2 hover:bg-blue-600 max-sm:text-[10px] max-sm:p-1 hover:text-white px-3 py-[0.4rem] ml-2 text-sm font-medium leading-normal items-center"
-                      >
-                        Resume Test 50
-                        <img src={coinimage} className="w-5" alt="coins" />
-                      </button>
-                    </>
-                  ) : (
-                    ""
-                  )}
+                    {item.paperprice === 0 ? (
+                      "FREE"
+                    ) : (
+                      <>
+                        {item.paperprice}
+                        <img
+                          src={coinimage}
+                          alt="coinimage"
+                          className="h-4 w-4"
+                        ></img>
+                      </>
+                    )}
+                  </div>
                 </div>
-                <div className="bg-green-300 max-sm:text-[10px] max-sm:py-0.5 max-sm:px-1.5  w-max px-2  text-green-900 rounded-2xl text-xs font-semibold flex items-center h-min py-[5px]">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth="1.5"
-                    stroke="currentColor"
-                    className="w-5 h-5 mr-1 max-sm:w-4 max-sm:h-4"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M21 11.25v8.25a1.5 1.5 0 0 1-1.5 1.5H5.25a1.5 1.5 0 0 1-1.5-1.5v-8.25M12 4.875A2.625 2.625 0 1 0 9.375 7.5H12m0-2.625V7.5m0-2.625A2.625 2.625 0 1 1 14.625 7.5H12m0 0V21m-8.625-9.75h18c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125h-18c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125Z"
-                    />
-                  </svg>
-                  {item.paperprice === 0
-                    ? "10% of marks rewarded as coins"
-                    : "15% of marks rewarded as coins"}
-                </div>
-              </div>
-            </div>
-          ))}
-        </div>
-        <Modal
-          style={{ backdropFilter: "blur(1px)" }}
-          open={open}
-          onClose={handleClose}
-          aria-labelledby="modal-modal-title"
-          aria-describedby="modal-modal-description"
-        >
-          <Box className="">
-            <div className="fixed top-0 left-0 flex items-center justify-center w-full h-full min-h-screen">
-              <div className="w-full max-w-[360px] max-sm:max-w-[300px]  bg-slate-50  rounded-sm text-center  ">
-                <div className="h-44 max-sm:h-40 flex  justify-center  items-center flex-col bg-gradient-to-br from-slate-800 via-slate-600 to-slate-700 ">
-                  <img src={coinimg} className="w-12 h-12" />
-                  <p className="text-xl text-yellow-400 font-semibold uppercase  ">
-                    Insufficient Elitecoins !!
+                <h5 className=" border-neutral-100 px-6 pt-2 md:text-xl text-sm font-medium leading-tight">
+                  {item.qpname}
+                </h5>
+                <div className="p-5 max-sm:p-3  flex  md:text-lg text-xs">
+                  <p className="px-1">{item.noofquestions} Questions</p>
+                  <p className="px-1 text-gray-400">|</p>
+                  <p className="px-1">{item.totalmarks} marks</p>
+                  <p className="px-1 text-gray-400">|</p>
+                  <p className="px-1">
+                    {humanizeDuration(item.examinfo?.duration)}
                   </p>
                 </div>
-                <div className="p-5">
-                  <p className=" text-base leading-relaxed text-gray-500 ">
-                    "Start the test smoothly with enough elite coins . Easily
-                    earn coins to unlock the test !"
-                  </p>
-                </div>
-                <div className="flex flex-wrap py-4   ">
-                  <div className="w-1/2 px-3">
+                <div className="mx-6 mb-3  flex items-center justify-between max-sm:flex-col max-sm:items-center   gap-2">
+                  <div className="flex">
                     <button
-                      onClick={handleClose}
-                      className="block w-full px-1 py-2 text-base font-normal text-center  border text-gray-500 bg-gray-100  hover:bg-gray-200  rounded-md "
+                      onClick={() => startTest(item)}
+                      type="button"
+                      className="inline-block rounded text-white bg-blue-500 hover:bg-blue-600 border-blue-500  border-2 px-4 py-2 text-sm max-sm:text-[10px] max-sm:p-1 leading-normal"
                     >
-                      Go Back
+                      {item.isResume ? "Start new test" : "Start Test"}
                     </button>
+                    {item.isResume ? (
+                      <>
+                        {" "}
+                        <button
+                          onClick={() => checkCoins(item, 50)}
+                          type="button"
+                          className="flex rounded text-blue-500 bg-white border-blue-500 border-2 hover:bg-blue-600 max-sm:text-[10px] max-sm:p-1 hover:text-white px-3 py-[0.4rem] ml-2 text-sm font-medium leading-normal items-center"
+                        >
+                          Resume Test 50
+                          <img src={coinimage} className="w-5" alt="coins" />
+                        </button>
+                      </>
+                    ) : (
+                      ""
+                    )}
                   </div>
-                  <div className="w-1/2 px-3">
-                    <Link to="/Dashboard/refer&earn">
-                      <button className=" flex justify-center gap-2 w-full px-1 py-2 text-base font-medium text-center transition border  hover:bg-slate-700 text-white bg-gradient-to-br from-slate-800 via-slate-600 to-slate-700 rounded-md ">
-                        Earn
-                        <img src={coinimg} className="w-6 h-6" />
-                      </button>
-                    </Link>
+                  <div className="bg-green-300 max-sm:text-[10px] max-sm:py-0.5 max-sm:px-1.5  w-max px-2  text-green-900 rounded-2xl text-xs font-semibold flex items-center h-min py-[5px]">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth="1.5"
+                      stroke="currentColor"
+                      className="w-5 h-5 mr-1 max-sm:w-4 max-sm:h-4"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M21 11.25v8.25a1.5 1.5 0 0 1-1.5 1.5H5.25a1.5 1.5 0 0 1-1.5-1.5v-8.25M12 4.875A2.625 2.625 0 1 0 9.375 7.5H12m0-2.625V7.5m0-2.625A2.625 2.625 0 1 1 14.625 7.5H12m0 0V21m-8.625-9.75h18c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125h-18c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125Z"
+                      />
+                    </svg>
+                    {item.paperprice === 0
+                      ? "10% of marks rewarded as coins"
+                      : "15% of marks rewarded as coins"}
                   </div>
                 </div>
               </div>
-            </div>
-          </Box>
-        </Modal>
-      </section>
+            ))}
+          </div>
+          <Modal
+            style={{ backdropFilter: "blur(1px)" }}
+            open={open}
+            onClose={handleClose}
+            aria-labelledby="modal-modal-title"
+            aria-describedby="modal-modal-description"
+          >
+            <Box className="">
+              <div className="fixed top-0 left-0 flex items-center justify-center w-full h-full min-h-screen">
+                <div className="w-full max-w-[360px] max-sm:max-w-[300px]  bg-slate-50  rounded-sm text-center  ">
+                  <div className="h-44 max-sm:h-40 flex  justify-center  items-center flex-col bg-gradient-to-br from-slate-800 via-slate-600 to-slate-700 ">
+                    <img src={coinimg} className="w-12 h-12" />
+                    <p className="text-xl text-yellow-400 font-semibold uppercase  ">
+                      Insufficient Elitecoins !!
+                    </p>
+                  </div>
+                  <div className="p-5">
+                    <p className=" text-base leading-relaxed text-gray-500 ">
+                      "Start the test smoothly with enough elite coins . Easily
+                      earn coins to unlock the test !"
+                    </p>
+                  </div>
+                  <div className="flex flex-wrap py-4   ">
+                    <div className="w-1/2 px-3">
+                      <button
+                        onClick={handleClose}
+                        className="block w-full px-1 py-2 text-base font-normal text-center  border text-gray-500 bg-gray-100  hover:bg-gray-200  rounded-md "
+                      >
+                        Go Back
+                      </button>
+                    </div>
+                    <div className="w-1/2 px-3">
+                      <Link to="/Dashboard/refer&earn">
+                        <button className=" flex justify-center gap-2 w-full px-1 py-2 text-base font-medium text-center transition border  hover:bg-slate-700 text-white bg-gradient-to-br from-slate-800 via-slate-600 to-slate-700 rounded-md ">
+                          Earn
+                          <img src={coinimg} className="w-6 h-6" />
+                        </button>
+                      </Link>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </Box>
+          </Modal>
+        </section>
+      ) : (
+        <LoadingSpinner />
+      )}
     </>
   );
 };
